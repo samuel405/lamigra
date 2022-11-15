3873b8303b9120489a0252a208cf443434d471f2
Samuel da Silva <samueldasilvadev@gmail.com>
2022-11-15 01:52:18 -0300

    create collection files

---
+++ constants.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MYSQL_HOST = String(process.env.MYSQL_HOST) || 'localhost';
+const MYSQL_PORT = Number(process.env.MYSQL_PORT) || 3306;
+const MYSQL_USER = String(process.env.MYSQL_USER) || '';
+const MYSQL_PASS = String(process.env.MYSQL_PASS) || '';
+const MYSQL_DB = String(process.env.MYSQL_DB) || '';
+const MONGO_URL = String(process.env.MONGO_URL) || '';
+const MONGO_DB = String(process.env.MONGO_DB) || '';
+
+module.exports = {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PASS,
+  MYSQL_DB,
+  MONGO_URL,
+  MONGO_DB
+};
--- mongo.js
+++ mongo.js
@@ -1,33 +1,32 @@
+const { MONGO_URL, MONGO_DB } = require('./constants');
 const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+const mongoConn = (new MongoClient(MONGO_URL)).db(MONGO_DB);
 
-// Database Name
-const dbName = 'streaming';
+module.exports = mongoConn;
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
+// // Database Name
+// const dbName = 'streaming';
 
-  const films = await db.collection("films");
+// async function main() {
+//   // Use connect method to connect to the server
+//   await client.connect();
+//   console.log('Connected successfully to server');
+//   const db = client.db(dbName);
+//   const collection = db.collection('documents');
 
-  films.insertOne({name: "Indiana Jones"});
+//   const films = await db.collection("films");
 
-  console.log(films.find({}));
+//   // films.insertOne({name: "Indiana Jones"});
 
-  // the following code examples can be pasted here...
+//   console.log(films.find({}));
 
-  return 'done.';
-}
+//   // the following code examples can be pasted here...
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+//   return 'done.';
+// }
+
+// main()
+//   .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close());
